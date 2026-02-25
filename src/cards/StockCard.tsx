742ec0b941e89af01e5f95ee4e5c817561c75753
이서준 <104981505+xeoxxn@users.noreply.github.com>
2026-02-25 16:38:36 +0900

    chore: imageUrl 추가

--- src/cards/StockCard.tsx
+++ src/cards/StockCard.tsx
@@ -3,13 +3,26 @@ import Medicine from "../assets/logo/Medicine.tsx";
 interface StockCardProps {
   title: string;
   leftNum: number;
+  imageUrl?: string;
 }
-export default function StockCard({ title, leftNum }: StockCardProps) {
+export default function StockCard({
+  title,
+  leftNum,
+  imageUrl,
+}: StockCardProps) {
   return (
     <div className="w-92 shrink-0 px-20 py-20 bg-white/20 rounded-[50px] flex justify-center items-center">
       <div className="flex flex-col items-center gap-12">
         <div className="w-48 h-48 p-5 bg-zinc-100 rounded-full flex justify-center items-center overflow-hidden">
-          <Medicine />
+          {imageUrl ? (
+            <img
+              src={imageUrl}
+              alt={title}
+              className="w-full h-full object-contain"
+            />
+          ) : (
+            <Medicine /> // fallback
+          )}
         </div>
 
         <div className="flex flex-col items-center gap-7 w-full">
