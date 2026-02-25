c79a337fe1a8c23a808dae1a71d4f6c36df5892f
이서준 <104981505+xeoxxn@users.noreply.github.com>
2026-02-25 16:35:07 +0900

    chore: 이름 관련 로직 수정

--- src/utils/displayName.ts
+++ src/utils/displayName.ts
@@ -1,5 +1,7 @@
 export function displayNameForItem(name: string) {
-  if (!name.startsWith("케이블(")) return name;
+  if (!(name.startsWith("케이블(") || name.startsWith("마스크("))) {
+    return name;
+  }
 
   const m = name.match(/\(([^)]+)\)/);
   return m ? m[1].trim() : name;
