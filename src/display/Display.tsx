2e95c4a14e4c12f254fe7c18a2e1142905fb559c
이서준 <104981505+xeoxxn@users.noreply.github.com>
2026-03-04 18:56:48 +0900

    fix: gap 너비 수정

--- src/display/Display.tsx
+++ src/display/Display.tsx
@@ -70,7 +70,7 @@ export default function Display() {
   }, [isError, error]);
 
   return (
-    <div className="w-full min-h-screen px-40 py-36 bg-gradient-to-l from-neutral-900 to-[#034E9D] flex justify-center">
+    <div className="w-full min-h-screen px-40 py-36 bg-gradient-to-l from-neutral-900 to-[#034E9D] flex justify-center items-center">
       <div className="w-[2703px] flex flex-col gap-20">
         {/* 헤더 */}
         <div className="w-full flex justify-between items-start">
@@ -98,120 +98,121 @@ export default function Display() {
             </div>
           </div>
         </div>
+        <div className="flex flex-col items-center gap-40">
+          {/* 행사 안내 */}
+          <div className="w-full flex flex-col gap-14">
+            <div className="text-white text-8xl font-semibold">행사 안내</div>
 
-        {/* 행사 안내 */}
-        <div className="w-full flex flex-col gap-14">
-          <div className="text-white text-8xl font-semibold">행사 안내</div>
-
-          <div className="w-full px-12 py-20 bg-white/10 rounded-[50px] flex justify-center items-center">
-            <div className="w-full">
-              <PosterCarousel
-                posters={
-                  posters.length > 0
-                    ? posters
-                    : [
-                        {
-                          posterId: "local-1",
-                          imageUrl: "src/assets/poster/poster1.jpeg",
-                        },
-                        {
-                          posterId: "local-2",
-                          imageUrl: "src/assets/poster/poster2.jpeg",
-                        },
-                        {
-                          posterId: "local-3",
-                          imageUrl: "src/assets/poster/poster3.jpeg",
-                        },
-                      ]
-                }
-                intervalMs={4000}
-                animMs={1500}
-              />
+            <div className="w-full px-12 py-20 bg-white/10 rounded-[50px] flex justify-center items-center">
+              <div className="w-full">
+                <PosterCarousel
+                  posters={
+                    posters.length > 0
+                      ? posters
+                      : [
+                          {
+                            posterId: "local-1",
+                            imageUrl: "src/assets/poster/poster1.jpeg",
+                          },
+                          {
+                            posterId: "local-2",
+                            imageUrl: "src/assets/poster/poster2.jpeg",
+                          },
+                          {
+                            posterId: "local-3",
+                            imageUrl: "src/assets/poster/poster3.jpeg",
+                          },
+                        ]
+                  }
+                  intervalMs={4000}
+                  animMs={1500}
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* 학사 일정 */}
-        <div className="w-full flex flex-col gap-14">
-          <div className="text-white text-8xl font-semibold">
-            {todayMonth}월 학사 일정
+          {/* 학사 일정 */}
+          <div className="w-full flex flex-col gap-14">
+            <div className="text-white text-8xl font-semibold">
+              {todayMonth}월 학사 일정
+            </div>
+
+            <div className="w-full px-12 py-20 bg-white/20 rounded-[50px] flex justify-center items-center gap-10">
+              {weekDays.map((item) => (
+                <WeekCard
+                  key={item.key}
+                  year={item.year}
+                  month={item.month}
+                  day={item.day}
+                  events={item.events}
+                  isToday={item.isToday}
+                />
+              ))}
+            </div>
           </div>
 
-          <div className="w-full px-12 py-20 bg-white/20 rounded-[50px] flex justify-center items-center gap-10">
-            {weekDays.map((item) => (
-              <WeekCard
-                key={item.key}
-                year={item.year}
-                month={item.month}
-                day={item.day}
-                events={item.events}
-                isToday={item.isToday}
-              />
-            ))}
+          {/* 재고 */}
+          <div className="w-full flex flex-col gap-14">
+            <div className="text-white text-8xl font-semibold">
+              복지물품 재고 현황
+            </div>
+            <StockAutoSlider stocks={stocks} />
           </div>
-        </div>
 
-        {/* 재고 */}
-        <div className="w-full flex flex-col gap-14">
-          <div className="text-white text-8xl font-semibold">
-            복지물품 재고 현황
-          </div>
-          <StockAutoSlider stocks={stocks} />
-        </div>
+          {/* 안내사항 */}
+          <div className="w-full flex flex-col gap-14">
+            <div className="text-white text-8xl font-semibold">안내사항</div>
 
-        {/* 안내사항 */}
-        <div className="w-full flex flex-col gap-14">
-          <div className="text-white text-8xl font-semibold">안내사항</div>
+            <div className="w-full px-12 py-20 bg-white/10 rounded-[50px] flex justify-between items-stretch gap-14">
+              {/* 왼쪽 */}
+              <div className="flex-1 flex flex-col gap-10">
+                <div className="w-full px-12 py-10 bg-white/10 rounded-[30px] flex flex-col gap-10">
+                  <div className="flex justify-between items-center">
+                    <div className="text-white text-7xl font-medium">
+                      복지물품 대여 시간
+                    </div>
+                    <div className="text-white text-6xl font-medium">
+                      10:00 ~ 17:00
+                    </div>
+                  </div>
 
-          <div className="w-full px-12 py-20 bg-white/10 rounded-[50px] flex justify-between items-stretch gap-14">
-            {/* 왼쪽 */}
-            <div className="flex-1 flex flex-col gap-10">
-              <div className="w-full px-12 py-10 bg-white/10 rounded-[30px] flex flex-col gap-10">
-                <div className="flex justify-between items-center">
-                  <div className="text-white text-7xl font-medium">
-                    복지물품 대여 시간
+                  <div className="text-white w-full text-5xl font-medium text-right">
+                    점심시간 (12:00~13:00) 제외
+                  </div>
+                </div>
+
+                <div className="w-full px-12 py-10 bg-white/10 rounded-[30px] flex justify-between items-center">
+                  <div className="text-white text-6xl font-medium">
+                    소프트웨어융합대학 교학팀
                   </div>
                   <div className="text-white text-6xl font-medium">
-                    10:00 ~ 17:00
+                    02-910-5047
                   </div>
                 </div>
-
-                <div className="text-white w-full text-5xl font-medium text-right">
-                  점심시간 (12:00~13:00) 제외
-                </div>
               </div>
 
-              <div className="w-full px-12 py-10 bg-white/10 rounded-[30px] flex justify-between items-center">
-                <div className="text-white text-6xl font-medium">
-                  소프트웨어융합대학 교학팀
+              {/* 오른쪽 */}
+              <div className="flex flex-shrink-0 items-center gap-14">
+                <div className="w-[554px] h-[718px] px-12 py-24 bg-white/10 rounded-[50px] flex flex-col justify-center items-center gap-20">
+                  <div className="text-center text-white text-7xl font-medium">
+                    빌릴게 접속
+                    <br /> QR코드
+                  </div>
+                  <img className="w-80 h-80" src="/Notion.svg" alt="qr코드" />
                 </div>
-                <div className="text-white text-6xl font-medium">
-                  02-910-5047
-                </div>
-              </div>
-            </div>
 
-            {/* 오른쪽 */}
-            <div className="flex flex-shrink-0 items-center gap-14">
-              <div className="w-[554px] h-[718px] px-12 py-24 bg-white/10 rounded-[50px] flex flex-col justify-center items-center gap-20">
-                <div className="text-center text-white text-7xl font-medium">
-                  빌릴게 접속
-                  <br /> QR코드
+                <div className="w-[554px] h-[718px] px-12 py-20 bg-white/10 rounded-[50px] flex flex-col justify-center items-center gap-20">
+                  <div className="w-[454px] text-center text-white text-7xl font-medium [text-shadow:_0px_4px_4px_rgb(0_0_0_/_0.25)]">
+                    학생회 오픈채팅
+                    <br />
+                    QR코드
+                  </div>
+                  <img
+                    className="w-80 h-80"
+                    src="/OpenChatting.svg"
+                    alt="오픈채팅 qr"
+                  />
                 </div>
-                <img className="w-80 h-80" src="/Notion.svg" alt="qr코드" />
-              </div>
-
-              <div className="w-[554px] h-[718px] px-12 py-20 bg-white/10 rounded-[50px] flex flex-col justify-center items-center gap-20">
-                <div className="w-[454px] text-center text-white text-7xl font-medium [text-shadow:_0px_4px_4px_rgb(0_0_0_/_0.25)]">
-                  학생회 오픈채팅
-                  <br />
-                  QR코드
-                </div>
-                <img
-                  className="w-80 h-80"
-                  src="/OpenChatting.svg"
-                  alt="오픈채팅 qr"
-                />
               </div>
             </div>
           </div>
