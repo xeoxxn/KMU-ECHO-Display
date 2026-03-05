8588351882abf76c12c229ae7e94ce1d828ecf87
이서준 <104981505+xeoxxn@users.noreply.github.com>
2026-03-05 19:16:14 +0900

    fix: 글자 로직 수정

--- src/utils/displayName.ts
+++ src/utils/displayName.ts
@@ -10,7 +10,7 @@ export function displayNameForItem(name: string) {
 
   result = result.replace("소형", "소").replace("대형", "대");
 
-  if (result.length > 7) {
+  if (result.length > 6) {
     result = result.slice(0, 4) + "...";
   }
 
